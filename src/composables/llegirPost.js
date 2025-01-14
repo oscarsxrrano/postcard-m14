43f03977e8fba9494eb967b88ee26d2a8ad00e05
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2025-01-14 20:02:58 +0100

    postID update

--- src/composables/llegirPost.js
+++ src/composables/llegirPost.js
@@ -2,21 +2,36 @@ import { ref } from "vue";
 
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
-export default function llegirPost(){
+export default function llegirPost() {
     // array reactiu para guardar mis posts
-    const posts  = ref([]);    
+    const posts = ref([]);
+    const post = ref(null);
+    const user = ref(null);
 
-    const llegirTots = async() => {
+    // funció per llegir els posts
+    const llegirTots = async () => {
         const response = await fetch(URL);
         posts.value = await response.json();
-    }
+    };
+
+    // funció per llegir els posts per ID
+    const llegirPostID = async (id) => {
+        const response = await fetch(`${URL}/${id}`);
+        post.value = await response.json();
+        
+        // obtenció de l'usuari relacionat amb el post
+        const userResponse = await fetch(`${URL}/users/${post.value.userId}`);
+        user.value = await userResponse.json();
+    };
 
     return {
         posts,
-        llegirTots
-    }
+        post,
+        user,
+        llegirTots,
+        llegirPostID,
+    };
 }
-
 
 
 
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,17 +1,27 @@
+<script setup>
+import { useRoute } from "vue-router";
+import llegirPost from "../composables/llegirPost.js";
+
+const route = useRoute();
+const { post, user, llegirPostID } = llegirPost();
+
+llegirPostID(route.params.id);
+</script>
+
 <template>
-  <div v-if="post && user">
+  <div v-if="post && user" class="p-4">
     <h1 class="text-3xl">{{ post.title }}</h1>
     <div class="text-gray-500 mb-10">by {{ user.name }}</div>
     <div>{{ post.body }}</div>
   </div>
+
+  <div v-else class="loading">
+    <p>Loading...</p>
+  </div>
 </template>
-<script setup>
-const post = {
-  title:
-    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
-};
-const user = {
-  name: "Leanne Graham",
-};
-</script>
+
+<style>
+  .loading {
+    cursor: wait;
+  }
+</style>
